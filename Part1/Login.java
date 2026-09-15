f2c24946ceab49205bea6469cd8ad118c14a3f85
Jeffrey Osborn <muomaifechidera1@gmail.com>
2026-09-15 12:25:11 +0200

    Add Login class skeleton with fields

---
+++ Part1/Login.java
@@ -0,0 +1,12 @@
+public class Login {
+
+    private String username;
+    private String password;
+    private String cellPhoneNumber;
+    private String firstName;
+    private String lastName;
+
+    public Login() {
+    }
+
+}
